3f225fa7d4a20a2e7b4124944f33c84d5f5eb347
Kamil Zemczak <kamilzemb@gmail.com>
2018-11-26 22:03:28 +0100

    Import

--- src/test/java/com/project/validator/StudentValidator_validateTest.java
+++ src/test/java/com/project/validator/StudentValidator_validateTest.java
@@ -23,7 +23,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.project.dao.StudentRepository;
